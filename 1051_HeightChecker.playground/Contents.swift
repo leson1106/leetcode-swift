9c5666eb0c01f41aaee354b5559b49b4107e3c50
Son Le <itsonla@ghtk.co>
2023-07-10 16:33:00 +0700

    1051 heights checker

--- 1051_HeightChecker.playground/Contents.swift
+++ 1051_HeightChecker.playground/Contents.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 class Solution {
-    func heightChecker(_ heights: [Int]) -> Int {
+    func oldOne(_ heights: [Int]) -> Int {
         var sorted = heights.sorted()
         
         var count: Int = 0
@@ -12,4 +12,26 @@ class Solution {
         
         return count
     }
+    
+    func heightChecker(_ heights: [Int]) -> Int {
+        var sorted = heights
+        
+        for i in stride(from: sorted.count - 1, to: 1, by: -1) {
+            for j in 0..<i where sorted[j] > sorted[j + 1] {
+                sorted.swapAt(j, j + 1)
+            }
+        }
+        
+        var count: Int = 0
+        for i in 0..<sorted.count {
+            if sorted[i] != heights[i] {
+                count += 1
+            }
+        }
+        return count
+    }
 }
+
+//var nums = [2,1,4,5,6,3]
+var nums = [1,1,4,2,1,3]
+let result = Solution().heightChecker(nums)
