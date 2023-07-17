730ee6dc49817f8f7715a427733c95af84c3ae3b
Son Le <itsonla@ghtk.co>
2023-07-17 22:11:47 +0700

    Added recursion way

--- LinkedList.playground/Pages/2_add_two_numbers.xcplaygroundpage/Contents.swift
+++ LinkedList.playground/Pages/2_add_two_numbers.xcplaygroundpage/Contents.swift
@@ -17,6 +17,8 @@ import Foundation
  Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
  Output: [8,9,9,9,0,0,0,1]
  */
+
+///Iteration
 extension Solution {
     func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
         var _l1 = l1
@@ -83,17 +85,31 @@ extension Solution {
     }
 }
 
-//let list1: ListNode? = ListNode(5, .init(6, .init(4, .init(9))))
-//let list2: ListNode? = ListNode(2, .init(4, .init(9)))
+///Recursion
+extension Solution {
+    func addTwoNumbers2(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        return _addTwoNumbers(l1, l2, carry: 0)
+    }
+    
+    func _addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?, carry: Int) -> ListNode? {
+        if l1 == nil && l2 == nil && carry == 0 { return nil }
+        let sum = (l1?.val ?? 0) + (l2?.val ?? 0) + carry
+        var carry = sum / 10
+        return ListNode(sum % 10, _addTwoNumbers(l1?.next, l2?.next, carry: carry))
+    }
+}
+
+let list1: ListNode? = ListNode(5, .init(6, .init(4, .init(9))))
+let list2: ListNode? = ListNode(2, .init(4, .init(9)))
 //let list1: ListNode? = ListNode(2, .init(4, .init(3)))
 //let list2: ListNode? = ListNode(5, .init(6, .init(4)))
-let list1: ListNode? = ListNode(9, .init(9, .init(9, .init(9))))
-let list2: ListNode? = ListNode(9, .init(9, .init(9, .init(9, .init(9, .init(9, .init(9)))))))
+//let list1: ListNode? = ListNode(9, .init(9, .init(9, .init(9))))
+//let list2: ListNode? = ListNode(9, .init(9, .init(9, .init(9, .init(9, .init(9, .init(9)))))))
 //let list1: ListNode? = ListNode(9, .init(9, .init(1)))
 //let list2: ListNode? = ListNode(1)
 //let list1: ListNode? = ListNode(3, .init(7))
 //let list2: ListNode? = ListNode(9, .init(2))
-let result = Solution().addTwoNumbers(list1, list2)
+let result = Solution().addTwoNumbers2(list1, list2)
 printNode(result)
 
 //: [Next](@next)
