6c4407edd40b24eacb0e8434a5d4846292c9d211
Son Le <itsonla@ghtk.co>
2023-07-19 22:11:21 +0700

    160

--- LinkedList.playground/Pages/160_intersection_of_2_linked_lists.xcplaygroundpage/Contents.swift
+++ LinkedList.playground/Pages/160_intersection_of_2_linked_lists.xcplaygroundpage/Contents.swift
@@ -3,8 +3,18 @@
 import Foundation
 
 extension Solution {
+    ///time: `O(m + n)` (m & n is length of headA/B)
+    ///space: `O(1)`
     func getIntersectionNode(_ headA: ListNode?, _ headB: ListNode?) -> ListNode? {
+        var a = headA
+        var b = headB
         
+        while a !== b {
+            a = (a == nil) ? headB : a?.next
+            b = (b == nil) ? headA : b?.next
+        }
+        
+        return a
     }
 }
 
