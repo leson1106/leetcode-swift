fe7b6b2fe058d3ca3998107659a014f16b1d2c4f
Son Le <itsonla@ghtk.co>
2023-08-19 21:58:29 +0700

    Intersection

--- Set_examples.playground/Contents.swift
+++ Set_examples.playground/Contents.swift
@@ -44,7 +44,40 @@ class Solution {
     func singleNumber(_ nums: [Int]) -> Int {
         nums.reduce(into: 0, ^=)
     }
+    
+    ///Given two integer arrays nums1 and nums2, return an array of their intersection.
+    ///Each element in the result must be unique and you may return the result in any order.
+    ///
+    ///     E.g: Input: nums1 = [1,2,2,1], nums2 = [2,2]
+    ///     Output: [2]
+    ///
+    ///     E.g: Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+    ///     Output: [9,4] | [4,9]
+    ///
+    ///Time complexity: linear time`O(n * m)` which n is length of nums1 & nums2
+    ///Space complexity: `O(n + m)` in worse case when all array's elements are different
+    func intersection(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+        let set1 = Set(nums1)
+        let set2 = Set(nums2)
+        
+        func iterate(_ biggerSet: Set<Int>, _ smallerSet: Set<Int>) -> [Int] {
+            var result: [Int] = []
+            for num in biggerSet {
+                if smallerSet.contains(num) {
+                    result.append(num)
+                }
+            }
+            return result
+        }
+        
+        if set1.count > set2.count {
+            return iterate(set1, set2)
+        } else {
+            return iterate(set2, set1)
+        }
+    }
 }
 
-let result1 = Solution().containsDuplicate([1,2,3,1])
-let result2 = Solution().singleNumber([1,1,2])
+//let result1 = Solution().containsDuplicate([1,2,3,1])
+//let result2 = Solution().singleNumber([1,1,2])
+let result3 = Solution().intersection([4,9,5], [9,4,9,8,4])
