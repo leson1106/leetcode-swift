552b094a4044177c9e1c22b4deab19a6e5384fe8
Son Le <itsonla@ghtk.co>
2023-09-22 22:32:14 +0700

    199 dfs

--- TreeNode.playground/Pages/199_binary_tree_right_side_view.xcplaygroundpage/Contents.swift
+++ TreeNode.playground/Pages/199_binary_tree_right_side_view.xcplaygroundpage/Contents.swift
@@ -29,13 +29,31 @@ extension Solution {
         }
         return res
     }
+    
+    ///`DFS`
+    func rightSideView2(_ root: TreeNode?) -> [Int] {
+        func dfs(_ r: TreeNode?, _ level: Int, _ res: inout [Int]) {
+            guard let r = r else { return }
+            if level >= res.count {
+                res.append(r.val)
+            } else {
+                res[level] = r.val
+            }
+            dfs(r.left, level + 1, &res)
+            dfs(r.right, level + 1, &res)
+        }
+        
+        var res: [Int] = []
+        dfs(root, 0, &res)
+        return res
+    }
 }
 
 let root: TreeNode? = .init(1, .init(2, nil, .init(5)), .init(3, nil, .init(4)))
 let root1: TreeNode? = .init(1, .init(2), nil)
 let root2: TreeNode? = .init(3, .init(9, .init(11), nil), .init(20))
-let result = Solution().rightSideView(root)
-let result1 = Solution().rightSideView(root1)
-let result2 = Solution().rightSideView(root2)
+let result = Solution().rightSideView2(root)
+let result1 = Solution().rightSideView2(root1)
+let result2 = Solution().rightSideView2(root2)
 
 //: [Next](@next)
