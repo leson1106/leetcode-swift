e0d684dd891101cd0ee55d210364d77edb886eb8
Son Le <itsonla@ghtk.co>
2023-10-22 18:39:03 +0700

    pascal triangle with formula

--- Array.playground/Pages/PascalTriangleII.xcplaygroundpage/Contents.swift
+++ Array.playground/Pages/PascalTriangleII.xcplaygroundpage/Contents.swift
@@ -3,6 +3,8 @@
 import Foundation
 
 class Solution {
+    ///Time complexity `O(rowIndex ^ 2)` which n is depth and m is breadth of triangle
+    ///Space `O(n + m)` where n is triangle elements and m is res
     func getRow(_ rowIndex: Int) -> [Int] {
         var triangle = [[Int]]()
         var res = [Int]()
@@ -23,10 +25,23 @@ class Solution {
         }
         return res
     }
+    
+    ///Following by formula: `arr[i] = arr[i - 1] * (row - i + 1) / i`
+    ///Time complexity `O(n)`
+    ///Space complexity `O(1)`
+    func getRow2(_ rowIndex: Int) -> [Int] {
+        guard rowIndex > 0 else { return [1] }
+        var res = Array(repeating: 1, count: rowIndex + 1)
+        for i in 1..<rowIndex {
+            res[i] = res[i - 1] * (rowIndex - i + 1) / i
+        }
+        return res
+    }
 }
 
-let res = Solution().getRow(0)
-let res1 = Solution().getRow(3)
-let res2 = Solution().getRow(5)
+let res = Solution().getRow2(0)
+let res1 = Solution().getRow2(3)
+let res2 = Solution().getRow2(4)
+let res3 = Solution().getRow2(5)
 
 //: [Next](@next)
