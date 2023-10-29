ee048021d56aaf5ccc8b19677c06ff0a8b9a43de
Son Le <itsonla@ghtk.co>
2023-10-29 21:12:53 +0700

    fibo

---
+++ 509_80_FibonacciFormat.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+class Solution {
+    private var cache: [Int: Int] = [:]
+    
+    ///Fibonacci with memoi
+    ///Time complexity `O(n^2)` cause every steps will split into 2 steps
+    ///Space Complexity `O(m + k)` with memoi, m is depth, k is map size
+    func fib(_ n: Int) -> Int {
+        if let num = cache[n] {
+            return num
+        }
+        if n > 1 {
+            let i = fib(n-1)
+            cache[n-1] = i
+            let j = fib(n-2)
+            cache[n-2] = j
+            return i + j
+        } else {
+            return n == 0 ? 0 : 1
+        }
+    }
+    
+    func climbStairs(_ n: Int) -> Int {
+        if let num = cache[n] {
+            return num
+        }
+        if n > 1 {
+            let i = climbStairs(n-1)
+            cache[n-1] = i
+            let j = climbStairs(n-2)
+            cache[n-2] = j
+            return i + j
+        } else {
+            return 1
+        }
+    }
+}
