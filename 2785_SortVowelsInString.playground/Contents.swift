35c7f91993bf8fb4fd4514849bc4ec14efbbdc08
Son Le <itsonla@ghtk.co>
2023-11-14 08:05:28 +0700

    2785

---
+++ 2785_SortVowelsInString.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class Solution {
+    ///Time complexity `O(n logn)`
+    ///Space complexity `O(m)` where m is length of total vowels in string
+    func sortVowels(_ s: String) -> String {
+        var s = s.map { String($0) }
+        var vowelsArray = [String]()
+        let vowels: [String] = ["a", "e", "i", "o", "u"]
+        for c in s where vowels.contains(c.lowercased()) {
+            vowelsArray.append(c)
+        }
+        
+        vowelsArray.sort()
+        var currIndex = 0
+        for i in 0..<s.count {
+            if vowels.contains(s[i].lowercased()) {
+                s[i] = vowelsArray[currIndex]
+                currIndex += 1
+            }
+        }
+        return s.joined()
+    }
+}
+
+let res = Solution().sortVowels(lEetcOde)
+let res1 = Solution().sortVowels(lYmpH)
